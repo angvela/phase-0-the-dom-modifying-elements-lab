142027e32f91e38a7596ffb9c0c74c72dad0aaa8
angvela <angelamusamali05@gmali.com>
2025-06-30 08:23:51 +0300

    Remove main element and add victory header

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Remove the <main> element
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set its ID and content
+newHeader.id = 'victory';
+newHeader.textContent = 'Angela is the champion';
+
+// Append it to the <body>
+document.body.append(newHeader);
